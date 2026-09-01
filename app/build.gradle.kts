a1e65efd28c05413d5ab456a74b479d05440b04a
hungnq131193-ux <hungnq131193@gmail.com>
2026-09-01 11:04:11 +0700

    chore(release): bump version to 1.0.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.hpre.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0.18"
+        versionCode = 20
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "com.hpre.app.testing.HPreTestRunner"
         vectorDrawables {
