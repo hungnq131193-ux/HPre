d080dd919d4cefa0235613fe6cd1d35b1350f9b3
hungnq131193-ux <hungnq131193@gmail.com>
2026-09-03 18:03:50 +0700

    chore(release): prepare HPre 1.0.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.hpre.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.0.24"
+        versionCode = 26
+        versionName = "1.0.25"
 
         testInstrumentationRunner = "com.hpre.app.testing.HPreTestRunner"
         vectorDrawables {
